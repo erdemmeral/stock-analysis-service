558228ebdd11e81483bc897a2cb9f79fcc049e33
Erdem Meral <erdem_meral@hotmail.com>
2025-02-11 12:14:54 +0300

    telegram

--- src/config.py
+++ src/config.py
@@ -11,15 +11,15 @@ FUNDAMENTAL_ANALYSIS_INTERVAL = 86400  # 24 hours in seconds
 PORTFOLIO_THRESHOLD_SCORE = int(os.getenv('PORTFOLIO_THRESHOLD_SCORE', '75'))
 
 # Telegram configuration
-TELEGRAM_BOT_TOKEN = '7530096691:AAEbEnXA2PJyihv0S3HD_5q1_pq6QRSj7-g'
-TELEGRAM_CHANNEL_ID = '-4678926083'
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
 
 # Portfolio API URL
-PORTFOLIO_API_URL = 'https://portfolio-tracker-rough-dawn-5271.fly.dev/api'
+PORTFOLIO_API_URL = os.getenv('PORTFOLIO_API_URL', 'https://portfolio-tracker-rough-dawn-5271.fly.dev/api')
 
 # API Keys
-ALPHA_VANTAGE_KEY = "your_alpha_vantage_key"
-FINNHUB_KEY = "your_finnhub_key"
-NEWS_API_KEY = "your_newsapi_key"
+ALPHA_VANTAGE_KEY = os.getenv('ALPHA_VANTAGE_KEY')
+FINNHUB_KEY = os.getenv('FINNHUB_KEY')
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 
 # Other configuration... 
